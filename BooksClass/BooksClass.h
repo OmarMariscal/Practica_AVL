f3c6b602d026735497a6b1f62308fd1b2ed73f0d
OmarMariscal <omarmariscal543@gmail.com>
2025-07-10 14:07:32 -0600

    Estructura de carpetas de la clase de Libros.

---
+++ BooksClass/BooksClass.h
@@ -0,0 +1,11 @@
+#ifndef BooksClass_H
+#define BooksClass_H
+
+#include <iostream>
+#include "Books_EXC.h"
+
+namespace BooksClass{
+
+}
+
+#endif
---
+++ BooksClass/Books_EXC.h
@@ -0,0 +1,11 @@
+#ifndef Books_EXC_H
+#define Books_EXC_H
+
+#include <iostream>
+#include <stdexcept>
+
+namespace Books_EXC{
+    
+}
+
+#endif
