6fb7162d040277545ec3da589b02c8a033ee9041
OmarMariscal <omarmariscal543@gmail.com>
2025-07-10 14:11:20 -0600

    Estructura de carpetas del AVL.

---
+++ AVL/AVL.h
@@ -0,0 +1,12 @@
+#ifndef AVL_H
+#define AVL_H
+
+namespace NodeAVL{
+
+}
+
+namespace AVL{
+    
+}
+
+#endif
---
+++ AVL/AVL_EXC.h
@@ -0,0 +1,11 @@
+#ifndef AVL_EXC_H
+#define AVL_EXC_H
+
+#include <iostream>
+#include <stdexcept>
+
+namespace AVL_EXC{
+    
+}
+
+#endif
